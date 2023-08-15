8cb17f0266a3ef9b8185d5877b5b636c052365cf
zaidi mounib <mounibzaidi@gmail.com>
2023-08-15 08:27:36 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" }
+    //...
+]
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
